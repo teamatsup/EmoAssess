ed885121796cc5e7cf924656c17f5c535d8f25ba
Roltaire Solis <solisroltaire@gmail.com>
2016-01-31 07:44:20 +0800

    registration working

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -47,7 +47,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{URL::to('/')}}" >Emo-Assessor</a>
+                <a class="navbar-brand" href="{{URL::to('/')}}" >Feel Me Out</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
